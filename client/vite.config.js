939e716e37db5c3a50176aed320d304fb766df4a
Abdullah-Saleh-Algaadi <abdullahsalehali46@gmail.com>
2026-07-10 21:03:17 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // أو أي إضافات أخرى تستخدمها
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-site/', // أضف هذا السطر واسم المستودع بين العلامتين
 })
